9de0071f48e535658d7bea8e5d2de5104f5719e4
koudaiii <cs006061@gmail.com>
2014-02-20 00:03:50 +0900

    kimatakubot lib/tasks/twitter.rake

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kimatakubot::Application.config.secret_key_base = '3d81864b702f4d91b474dae64847aae56c88e8432b3c8e0f2529c2aca9ab4d9621c38ff575d9e152ce6efd8c688c708f1d2e2e828a20ddab325b9669d44e506d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kimatakubot::Application.config.session_store :cookie_store, key: '_kimatakubot_session'
